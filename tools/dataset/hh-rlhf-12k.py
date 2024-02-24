f39e2c07d453b586c467d571326d185dad59ab45
kazuki <kaz.tokyo.tech20@gmail.com>
2024-02-25 01:31:22 +0900

    fix: hh-rlhf convert script

--- tools/dataset/hh-rlhf-12k.py
+++ tools/dataset/hh-rlhf-12k.py
@@ -48,7 +48,56 @@ def main() -> None:
                 print(f"invalid conversation={conversation}")
 
         instruction_conversation["output"] = conversations["chosen"]
-        instruction_data.append(instruction_conversation)
+        if len(instruction_conversation["output"]) < 1:
+            # delete black output case
+            print("DEBUG: skip " + instruction_conversation["output"])
+        elif instruction_conversation["output"] in [
+            "[本文省略］", '"""', "..........", '"****!"', 'もし'
+        ]:
+            print("DEBUG: skip " + instruction_conversation["output"])
+        else:
+            if len(instruction_conversation["output"]) <= 10:
+                print("DEBUG: " + instruction_conversation["output"])
+
+            if instruction_conversation["output"] in [
+                "どういたしまして。", "どういたしまして！"
+            ]:
+                instruction_conversation["output"] = "どういたしまして"
+
+            if instruction_conversation["output"] == "なぜそう思うんだ？":
+                instruction_conversation["output"] = "なぜ、そう思うのですか？"
+
+            if instruction_conversation["output"] == "もっと文脈が必要だ":
+                instruction_conversation["output"] = "回答するには、さらに文脈が必要です。情報を追加してください。"
+
+            if instruction_conversation["output"] == "待って、何に使うの？":
+                instruction_conversation["output"] = "待ってください。何に使うのでしょうか？"
+
+            if instruction_conversation["output"] == "わからないよ。":
+                instruction_conversation["output"] = "すみません。わかりません。"
+
+            if instruction_conversation["output"] == "どういう意味ですか？":
+                instruction_conversation["output"] = "どういう意味ですか？詳細に教えて下さい。"
+
+            if instruction_conversation["output"] == "どれ？":
+                instruction_conversation["output"] = "どれでしょうか？"
+
+            if instruction_conversation["output"] == "他に何をしたい？":
+                instruction_conversation["output"] = "他に何かしたいことはあるでしょうか？"
+
+            if instruction_conversation["output"] == "何を話したいの？":
+                instruction_conversation["output"] = "何について話したいのでしょうか？"
+
+            if instruction_conversation["output"] == "説明してみようか。":
+                instruction_conversation["output"] = "説明してみましょうか？"
+
+            if instruction_conversation["output"] == "どうする？":
+                instruction_conversation["output"] = "どうしましょうか？"
+
+            instruction_conversation["chosen"] = instruction_conversation["output"]
+            instruction_conversation["rejected"] = conversations["rejected"]
+
+            instruction_data.append(instruction_conversation)
 
     print(f"\n\nlen(instruction_data)={len(instruction_data)}")
 
