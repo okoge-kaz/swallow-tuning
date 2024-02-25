bc87e217a6f5f377b0eecc7980f231d0d3262b40
kazuki <kaz.tokyo.tech20@gmail.com>
2024-02-26 02:40:48 +0900

    feat: exclude 0 length input

--- tools/dataset/oasst1-21k-ja.py
+++ tools/dataset/oasst1-21k-ja.py
@@ -123,6 +123,9 @@ def is_invalid_input(conversation: dict) -> bool:
         elif len(conversation["input"]) == 1:
             if conversation["input"][0]["text"] == "":
                 return True
+        elif any(len(role_text["text"]) == 0 for role_text in conversation["input"]):
+            return True
+
         return False
     else:
         return True
