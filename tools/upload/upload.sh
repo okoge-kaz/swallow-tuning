513f304c5e1c06c18b781825158c37aefa28f75f
kazuki <kaz.tokyo.tech20@gmail.com>
2024-03-04 17:52:24 +0900

    chore: upload 7B NEFTune models

--- tools/upload/upload.sh
+++ tools/upload/upload.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-start=2430
-end=2430
+start=866
+end=866
 increment=5000
 
-upload_base_dir=/bb/llm/gaf51275/llama/checkpoints/Swallow-7b-VE-instruct-v1-NEFTune/baseline-lr_2e-5-minlr_2e-6
+upload_base_dir=/bb/llm/gaf51275/llama/checkpoints/Swallow-7b-VE-instruct-v1-NEFTune/baseline-imitation_2-lr_2e-5-minlr_2e-6-GB_256
 
 for ((i = start; i <= end; i += increment)); do
   upload_dir=$upload_base_dir/checkpoint-$i
 
   python tools/upload/upload.py \
     --ckpt-path $upload_dir \
-    --repo-name tokyotech-llm/Swallow-7b-VE-instruct-v1.0-NEFTune-baseline-lr_2e-5-minlr_2e-6-iter$(printf "%07d" $i)
+    --repo-name tokyotech-llm/Swallow-7b-VE-NEFTune-v1.0-baseline-imitation_2-iter$(printf "%07d" $i)
 done
