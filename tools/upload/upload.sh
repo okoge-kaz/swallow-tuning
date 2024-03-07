64a14ba57af7809a4c0c05ddb7b402c7f5e53a14
kazuki <kaz.tokyo.tech20@gmail.com>
2024-03-07 18:03:50 +0900

    chore: update ckpt iteration

--- tools/upload/upload.sh
+++ tools/upload/upload.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-start=866
-end=866
+start=2154
+end=2154
 increment=5000
 
-upload_base_dir=/bb/llm/gaf51275/llama/converted-hf-checkpoint/Swallow-13b-VE-NEFTune/baseline-imitation_2-lr_2e-5-minlr_2e-6-GB_256
+upload_base_dir=/bb/llm/gaf51275/llama/converted-hf-checkpoint/Swallow-13b-VE-instruct/hh-rlhf-dolly-ossat-imitation-lr_2e-5-minlr_2e-6-GB128
 
 for ((i = start; i <= end; i += increment)); do
   upload_dir=$upload_base_dir/iter_$(printf "%07d" $i)
 
   python tools/upload/upload.py \
     --ckpt-path $upload_dir \
-    --repo-name tokyotech-llm/Swallow-13b-VE-NEFTune-v1.0-baseline-imitation_2-lr_2e-5-minlr_2e-6-GB_256-iter$(printf "%07d" $i)
+    --repo-name tokyotech-llm/Swallow-13b-VE-instruct-v1.0-hh-rlhf-dolly-ossat-imitation-lr_2e-5-minlr_2e-6-GB128-iter$(printf "%07d" $i)
 done
