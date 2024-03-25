d5a1fd132916a4216d30b41212a6d7d2415210db
kazuki <kaz.tokyo.tech20@gmail.com>
2024-03-26 00:46:25 +0900

    feat: baseline imitation-2-3

--- tools/dataset/merge_and_split.sh
+++ tools/dataset/merge_and_split.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 INPUT_DIR=/bb/llm/gaf51275/llama/finetuning/datasets/formatted
-OUTPUT_DIR=/bb/llm/gaf51275/llama/finetuning/datasets/training/hh-rlhf-dolly-ossat-imitation
+OUTPUT_DIR=/bb/llm/gaf51275/llama/finetuning/datasets/training/baseline-imitation-2-3
 
 mkdir -p $OUTPUT_DIR
 
 FILES_TO_MERGE=(
   "databricks-dolly-15k-en.jsonl"
   "databricks-dolly-15k-ja.jsonl"
-  "oasst2-top1-en.jsonl"
   "oasst1-21k-en.jsonl"
-  "hh-rlhf-12k-ja.jsonl"
+  "oasst1-21k-ja.jsonl"
   "oasst1-21k-ja-mixtral-imitation_2.jsonl"
   "convert_oasst1-21k-ja-mixtral-imitation_3.jsonl"
 )
+
+# reset merged.jsonl
+rm "$OUTPUT_DIR/merged.jsonl"
 
 # merge files
 for FILE in "${FILES_TO_MERGE[@]}"; do
